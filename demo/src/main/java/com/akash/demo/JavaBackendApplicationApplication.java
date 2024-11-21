8bd572eb55f10ebc1addfb2b47b7d14e7b03c1db
sudhakar <sudhakarv@emproto.com>
2024-11-21 13:23:13 +0530

    initial commit

---
+++ demo/src/main/java/com/akash/demo/JavaBackendApplicationApplication.java
@@ -0,0 +1,13 @@
+package com.akash.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendApplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendApplicationApplication.class, args);
+	}
+
+}
